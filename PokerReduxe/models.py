4456057deb25903ebeef013c79e3bf2cef22de67
Jared Cain <jared.cain77@gmail.com>
2016-07-20 13:57:17 -0700

    Add table file

---
+++ PokerReduxe/models.py
@@ -0,0 +1,17 @@
+from django import models
+
+
+class Table(models.Model):
+    pass
+
+class Player(models.Model):
+    pass
+
+class Seat(models.Model):
+    pass
+
+class Card(models.Model):
+    pass
+
+class Deck(models.Model):
+    pass
