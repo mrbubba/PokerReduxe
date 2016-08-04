c46d223fde2f75a4015f1cd2fc381e4b38ecafe7
Jared Cain <jared.cain77@gmail.com>
2016-08-04 16:34:33 -0700

    WIP add move button logic and tests

--- PokerReduxe/gamelogic/tests/test_app.py
+++ PokerReduxe/gamelogic/tests/test_app.py
@@ -23,6 +23,10 @@ class TestApp(unittest.TestCase):
         self.table.join(5, self.player5, 100)
         self.table.join(6, self.player6, 100)
 
+        # Set player order; happy path
+        for k,v in self.table.seats.items():
+            self.table.player_order.append(v)
+
     def test_get_active_players(self):
         """ Can we grab a list of active players? """
         six_players = app.get_active_players(self.table)
@@ -40,6 +44,56 @@ class TestApp(unittest.TestCase):
         call5 = app.reset_player_order(self.table)[:]
 
         self.assertFalse(call1 == call2 == call3 == call4 == call5)
+
+    def test_move_button(self):
+        """ Can we properly increment through the player order? """
+        expected = self.table.player_order[:]
+        x = expected.pop(0)
+        expected.append(x)
+        app.move_button(self.table)
+        self.assertEqual(expected, self.table.player_order)
+
+    def test_remove_inactive_from_hand(self):
+        """ Can we remove an inactive player from a hand """
+        self.player4.active = False
+        app.move_button(self.table)
+        self.assertEqual(len(self.table.player_order), 5)
+
+    def test_add_player_to_hand(self):
+        """ Can we add a new player to hand appropriately """
+        expected = self.table.player_order[:]
+        x = expected.pop(0)
+        expected.append(x)
+        x = expected.pop(0)
+        expected.append(x)
+        self.player4.active = False
+        app.move_button(self.table)
+        self.player4.active = True
+        app.move_button(self.table)
+        self.assertEqual(len(self.table.player_order), 6)
+        self.assertEqual(expected, self.table.player_order)
+
+    def test_bb_mia(self):
+        """ Can we prepend sb position to None if bb goes mia """
+        self.table.player_order[1].active = False
+        app.move_button(self.table)
+        self.assertEqual(self.table.player_order[0], None)
+        expected = self.table.player_order[:]
+        expected.pop(0)
+        app.move_button(self.table)
+        self.assertEqual(expected, self.table.player_order)
+
+    def test_set_missed_bb(self):
+        """ Can we set missed bb to True """
+        self.player3.active = False
+        app.move_button(self.table)
+        self.assertTrue(self.player3.missed_bb)
+
+    def test_set_missed_sb(self):
+        """ Can we set missed sb to True """
+        self.player2.active = False
+        app.move_button(self.table)
+        self.assertTrue(self.player2.missed_sb)
 
 if __name__ == '__main__':
     unittest.main()
