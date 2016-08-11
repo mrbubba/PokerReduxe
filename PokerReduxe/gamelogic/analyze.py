4547bfabd9fa03b6016e7f281d9ece56c26d23cd
Mark Fenter <bubbashipwreck@gmail.com>
2016-08-10 18:44:38 -0700

    WIP stubbed out the analyze function so that tests would pass

---
+++ PokerReduxe/gamelogic/analyze.py
@@ -0,0 +1,6 @@
+from table import Table
+
+
+def analyze(table):
+    """"analyze is called at the end of a hand.  It determines who and awards each pot to the player/s with the best hand"""
+    pass
