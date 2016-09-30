1605bb6901630ec092b373fb043f5d484d7a3293
Mark Fenter <bubbashipwreck@gmail.com>
2016-09-30 12:47:00 -0700

    add flask_layer directory

--- PokerReduxe/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-
-
-def deserialize(payload):
-    data = JSONParser().parse(payload)
-    return data
-
-json.dumps(payload)
--- README.md
+++
@@ -1,3 +0,0 @@
-# PokerReduxe
-
-This is a poker game/AI project written with python and javascript.
